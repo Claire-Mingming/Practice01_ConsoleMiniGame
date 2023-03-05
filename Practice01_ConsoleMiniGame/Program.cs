20e671adbd74d4156690d0e5ebac1bc1c076f4c2
ClaireMilk <glittlewest@gmail.com>
2023-03-05 10:44:42 +0000

    create project

---
+++ Practice01_ConsoleMiniGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice01_ConsoleMiniGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
